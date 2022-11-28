11f8dee09a3f2b83c6bdbaf895c25199a17d6ba4
Dragan Golic <golicdragan0@gmail.com>
2022-11-27 20:39:20 -0500

    only image

---
+++ main.py
@@ -0,0 +1,16 @@
+import fitz
+import PIL.Image
+import io
+
+pdf = fitz.open("sample.pdf")
+counter = 1
+for i in range(len(pdf)):
+    page = pdf[i]
+    images = page.get_images()
+    for image in images:
+        base_img = pdf.extract_image(image[0])
+        image_data = base_img["image"]
+        img = PIL.Image.open(io.BytesIO(image_data))
+        extension = base_img["ext"]
+        img.save(open(f"image{counter}.{extension}", "wb"))
+        counter += 1
